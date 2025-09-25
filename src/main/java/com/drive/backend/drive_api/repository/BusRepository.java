1cf1c39a1f407c613d9febacb17340eeb7b76e9e
KoEunsuk <koeunsuk12@gmail.com>
2025-09-26 02:45:01 +0900

    리팩토링

--- src/main/java/com/drive/backend/drive_api/repository/BusRepository.java
+++ src/main/java/com/drive/backend/drive_api/repository/BusRepository.java
@@ -1,7 +1,6 @@
 package com.drive.backend.drive_api.repository;
 
 import com.drive.backend.drive_api.entity.Bus;
-import com.drive.backend.drive_api.entity.Operator;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
