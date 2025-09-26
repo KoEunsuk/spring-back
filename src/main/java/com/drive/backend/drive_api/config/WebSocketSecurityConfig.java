2e920671c85910a8689d43060f1681875348646a
KoEunsuk <koeunsuk12@gmail.com>
2025-09-27 03:29:06 +0900

    웹소켓 - CONNECT는 허용하도록 수정

--- src/main/java/com/drive/backend/drive_api/config/WebSocketSecurityConfig.java
+++ src/main/java/com/drive/backend/drive_api/config/WebSocketSecurityConfig.java
@@ -11,26 +11,22 @@ public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBro
     @Override
     protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
         messages
-                // 1. CONNECT 요청은 인증만 되면 허용 (StompHandler에서 인증 처리)
+                // 1. CONNECT 요청은 일단 허용 (StompHandler에서 인증 처리)
                 //          만약 StompHandler에서 인증 실패 시 여기서 막히게 됨
-                .simpTypeMatchers(SimpMessageType.CONNECT).authenticated()
+                .simpTypeMatchers(SimpMessageType.CONNECT).permitAll()
 
+                // StompHandler에서 인증이 성공한 후에야, 아래의 실제 메시지/구독 규칙들이 의미를 갖게 됨
                 // 2. `/app/drive-events` 경로로 메시지를 보내는 것은 DRIVER 역할만 가능
                 .simpDestMatchers("/app/drive-events").hasRole("DRIVER")
 
                 // 3. 관리자가 경고를 구독하는 `/topic/operator/**` 경로는 ADMIN 역할만 가능
                 .simpSubscribeDestMatchers("/topic/operator/**").hasRole("ADMIN")
 
-                // 4. 그 외 다른 모든 메시지 발행 및 구독은 인증만 되면 허용
-                .simpTypeMatchers(SimpMessageType.MESSAGE, SimpMessageType.SUBSCRIBE).authenticated()
-
-                // 5. 명시적으로 허용하지 않은 다른 모든 것은 거부
-                .anyMessage().denyAll();
+                // 4. 명시적으로 허용하지 않은 것은 인증만 되었다면 일단 허용
+                .anyMessage().authenticated();
     }
 
-    /**
-     * CSRF 토큰 검사를 웹소켓에 대해서는 비활성화합니다.
-     */
+    // CSRF 토큰 검사를 웹소켓에 대해서는 비활성화합니다.
     @Override
     protected boolean sameOriginDisabled() {
         return true;
