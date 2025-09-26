dc245247e207a34060d73d562dcd239bf429d7e4
KoEunsuk <koeunsuk12@gmail.com>
2025-09-26 16:12:44 +0900

    이름변경

--- src/main/java/com/drive/backend/drive_api/controller/AdminDriverController.java
+++ src/main/java/com/drive/backend/drive_api/controller/AdminDriverController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RequestMapping("/api/admin/drivers")
 @PreAuthorize("hasRole('ADMIN')")
 @RequiredArgsConstructor
-public class DriverController {
+public class AdminDriverController {
 
     private final DriverService driverService;
 
