952a58d4d1f22bf9f57a8bf596fabb0ac0c79ec4
KoEunsuk <koeunsuk12@gmail.com>
2025-09-27 03:13:35 +0900

    자잘 수정

--- src/main/java/com/drive/backend/drive_api/config/WebSocketSecurityConfig.java
+++ src/main/java/com/drive/backend/drive_api/config/WebSocketSecurityConfig.java
@@ -11,17 +11,20 @@ public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBro
     @Override
     protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
         messages
-                // 1. `/app/drive-events` 경로로 메시지를 '보내는(destination)' 것은 DRIVER 역할만 가능
+                // 1. CONNECT 요청은 인증만 되면 허용 (StompHandler에서 인증 처리)
+                //          만약 StompHandler에서 인증 실패 시 여기서 막히게 됨
+                .simpTypeMatchers(SimpMessageType.CONNECT).authenticated()
+
+                // 2. `/app/drive-events` 경로로 메시지를 보내는 것은 DRIVER 역할만 가능
                 .simpDestMatchers("/app/drive-events").hasRole("DRIVER")
 
-                // 2. 관리자가 경고를 '구독하는(subscribe)' `/topic/operator/**` 경로는 ADMIN 역할만 가능
+                // 3. 관리자가 경고를 구독하는 `/topic/operator/**` 경로는 ADMIN 역할만 가능
                 .simpSubscribeDestMatchers("/topic/operator/**").hasRole("ADMIN")
 
-                // 3. 웹소켓 연결 자체(CONNECT)나 단순 메시지(MESSAGE)는 인증만 되면 허용
-                //    (위에서 지정한 특정 경로 외의 일반적인 통신)
-                .simpTypeMatchers(SimpMessageType.CONNECT, SimpMessageType.MESSAGE, SimpMessageType.SUBSCRIBE).authenticated()
+                // 4. 그 외 다른 모든 메시지 발행 및 구독은 인증만 되면 허용
+                .simpTypeMatchers(SimpMessageType.MESSAGE, SimpMessageType.SUBSCRIBE).authenticated()
 
-                // 4. 명시적으로 허용하지 않은 다른 모든 것은 거부하여 보안 강화
+                // 5. 명시적으로 허용하지 않은 다른 모든 것은 거부
                 .anyMessage().denyAll();
     }
 
