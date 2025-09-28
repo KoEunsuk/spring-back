eaa858ae23e59b8c0720c67f8f6041194264655a
KoEunsuk <koeunsuk12@gmail.com>
2025-09-29 00:30:05 +0900

    알림 및 경고 구현

---
+++ src/main/java/com/drive/backend/drive_api/enums/NotificationType.java
@@ -0,0 +1,6 @@
+package com.drive.backend.drive_api.enums;
+
+public enum NotificationType {
+    DRIVING_WARNING,    // 운행 중 경고
+    NEW_DISPATCH_ASSIGNED // 신규 배차 할당
+}
--- src/main/java/com/drive/backend/drive_api/repository/AdminRepository.java
+++ src/main/java/com/drive/backend/drive_api/repository/AdminRepository.java
@@ -2,8 +2,12 @@ package com.drive.backend.drive_api.repository;
 import com.drive.backend.drive_api.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    // Admin 중에서 특정 operator에 속한 사용자 찾기
+    List<Admin> findAllByOperator_OperatorId(Long operatorId);
 }
